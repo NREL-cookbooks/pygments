1e851532c8e7e5a9f995b3ce817731f410c7a298
Nick Muerdter <nick.muerdter@nrel.gov>
2014-04-09 16:28:46 -0600

    Adding name to metadata.

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
+name             "pygments"
 maintainer       "NREL"
 maintainer_email "nick.muerdter@nrel.gov"
 license          "All rights reserved"
 description      "Installs/Configures pygments"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
+version          "0.0.2"
 
 depends "python"
